5b800f99e64a98e479bbba4f86497bf6691c8eba
TristanPere <119620785+TristanPere@users.noreply.github.com>
2023-03-20 12:41:07 +0000

    Add SpringBoot files

---
+++ Java_SpringBoot_Backend/src/main/java/com/example/Java_SpringBoot_Backend/JavaSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_SpringBoot_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ Java_SpringBoot_Backend/src/test/java/com/example/Java_SpringBoot_Backend/JavaSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_SpringBoot_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
